3792a6ac4daf3b07a613cd9d07e686ab46cf124c
Ryo <kanadajinlee@yahoo.ca>
2016-12-18 15:30:41 -0500

    Trying to get MUI tabs to work

--- src/containers/FilmWall.jsx
+++ src/containers/FilmWall.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import FilmRow from './FilmRow.jsx';
 import DetailedCard from './DetailedCard';
+// import MyComponent from '../components/DetailedTabs'
 // const data = require('./media.json');
-
-// const movies = data.movies; //put in state
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// injectTapEventPlugin();
 
 class FilmWall extends Component {
 
@@ -15,6 +16,7 @@ class FilmWall extends Component {
         <FilmRow moviesTest={this.props.movies}/>
 
         <DetailedCard/>
+        {/* <MyComponent/> */}
       </div>
     );
   }
