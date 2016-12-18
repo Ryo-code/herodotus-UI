d427dee45e66e21b58a10d021bdc82777aec999a
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-18 02:12:34 +0000

    Can now display all posters in the row

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React, {Component} from 'react';
 import FilmWall from './containers/FilmWall.jsx';
 import NavBar from './containers/NavBar.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import axios from 'axios';
 
 import './App.css';
 import 'flexboxgrid';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
 injectTapEventPlugin();
 
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: []
+    }
+  }
 
+  componentDidMount() {
+    axios.get('http://0.0.0.0:3001/movies')
+      .then((response) => {
+        this.setState({ movies: response.data })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
     return (
       <MuiThemeProvider>
 
@@ -19,7 +37,7 @@ class App extends Component {
 
         <NavBar/>
 
-        <FilmWall/>
+        <FilmWall movies={this.state.movies} />
 
         <div>
           ~ ~ ~ ~ ~ {/* For when you click on it...*/}
--- src/containers/FilmWall.jsx
+++ src/containers/FilmWall.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import FilmRow from './FilmRow.jsx';
 import DetailedCard from './DetailedCard';
-const data = require('./media.json');
+// const data = require('./media.json');
 
-const movies = data.movies; //put in state
+// const movies = data.movies; //put in state
 
 class FilmWall extends Component {
   render() {
     return (
       <div className="container">
 
-        <FilmRow movies={movies.slice(0, 4)}/>
-        <FilmRow movies={movies.slice(4, 8)}/>
+        <FilmRow moviesTest={this.props.movies}/>
+        <FilmRow moviesTest={this.props.movies}/>
 
 
         <DetailedCard
