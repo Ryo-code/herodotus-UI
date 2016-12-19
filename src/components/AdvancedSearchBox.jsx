eec739970a64c5546dc17f1e2da21a7615bd398f
Joao Coelho <joaocoelho93@gmail.com>
2016-12-19 22:58:17 +0000

    improved search by title feature, and began implementing advanced search feature

---
+++ src/components/AdvancedSearchBox.jsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class AdvancedSearchBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {title: '',
+                  genre: '',
+                  keywords: '',
+                  date: ''
+                  }
+  }
+
+  handleSubmit = (event) => {
+    // event.preventDefault();
+    console.log(event.target);
+
+    const {title, genre, keywords, date} = this.state;
+
+    axios.get(`http://0.0.0.0:3001/movies/title?=${title}&genre=${genre}&keywords=${keywords}&date=${date}`)
+    .then((response) => {
+      this.props.updateMoviesFromSearchResult(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  }
+
+  handleFormChange = (event) => {
+    const {value, name} = event.target;
+    // const value = event.target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+
+
+  render(){
+    const { title, genre, keywords, date } = this.state;
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Advanced Search
+        </label>
+
+        <label>
+          Title:
+        </label>
+          <input onChange={this.handleFormChange} type="text" name="title" value={title}/>
+        <label>
+          Genre:
+        </label>
+          <input onChange={this.handleFormChange} type="text" name="genre" value={genre}/>
+        <label>
+          Keyword:
+        </label>
+          <input onChange={this.handleFormChange} type="text" name="keywords" value={keywords}/>
+        <label>
+         Date:
+        </label>
+          <input onChange={this.handleFormChange} type="text" name="date" value={date}/>
+          <input type="submit" value="Search" />
+      </form>
+    );
+
+  }
+
+}
+
+export default AdvancedSearchBox;
+// On form submit
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,37 @@
 import React, {Component} from 'react';
 import axios from 'axios'
+import _ from 'lodash'
 
 class SearchBar extends Component {
-
-
-
 
   constructor(props) {
     super(props);
     this.state = {value: ''};
+    this.makeRequest = _.debounce(this.makeRequest, 0)
   }
 
     // this.handleChange = this.handleChange.bind(this);
     // this.handleSubmit = this.handleSubmit.bind(this);
 
+    makeRequest = (search = this.state.value) => {
+      axios.get('http://0.0.0.0:3001/titles?name=' +search)
+        .then((response) => {
+          // console.log(response.data)
+          //UPDATE MOVIE METHOD
+          this.props.updateMoviesFromSearchResult(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
     handleChange = (event) => {
-      this.setState({value: event.target.value});
+      this.setState({value: event.target.value}, () =>{
+        console.log(this.state.value)
+        this.makeRequest();
+      });
+
+
     }
 
     handleQuerySumbit = (event) => {
@@ -23,35 +39,27 @@ class SearchBar extends Component {
         let search = this.state.value
         console.log("The query title is: " + search)
 
-        axios.get('http://0.0.0.0:3001/titles?name=' +search)
-          .then((response) => {
-            console.log(response.data)
-            //UPDATE MOVIE METHOD
-            this.props.updateMoviesFromSearchResult(response.data)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
+        this.makeRequest(search)
 
         this.setState({value: ""});
       }
     }
 
 
-
-
-
   render() {
 
     return(
-      <label>
-        Search By title:
-        <input type="text" name="query"
-           value={this.state.value}
-           onChange={this.handleChange}
-           onKeyUp={this.handleQuerySumbit}
-           />
-      </label>
+      <div>
+        <label>
+          Search By title:
+          <input type="text" name="query"
+             value={this.state.value}
+             onChange={this.handleChange}
+             onKeyUp={this.handleQuerySumbit}
+             />
+        </label>
+        <span>Advanced Search</span>
+      </div>
     );
 
   }
