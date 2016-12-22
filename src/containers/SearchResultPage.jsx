764972b9631f254b530ef1c12a2b6c226c589993
Joao Coelho <joaocoelho93@gmail.com>
2016-12-22 16:35:44 +0000

    correctly placed film search button in the nav bar and, added nav bar to the search reults page

--- src/containers/SearchResultPage.jsx
+++ src/containers/SearchResultPage.jsx
@@ -3,6 +3,7 @@ import SearchFilmRow from './SearchFilmRow.jsx';
 import axios from 'axios'
 import {Link} from 'react-router'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './NavBar.jsx';
 
 
 export default props => {
@@ -52,10 +53,15 @@ console.log(props.searchResults)
     return (
       <MuiThemeProvider>
         <div>
+        <NavBar
+          updateMoviesFromSearchResult={props.updateMoviesFromSearchResult}
+          updateToSearchResults={props.updateToSearchResults}
+        />
           <h1>
             <Link to="/">Back to Home</Link>
           </h1>
           <h1>Here are your search results</h1>
+          <h2>Your search has yielded {props.searchResults.length} results</h2>
           <SearchFilmRow
             currentMovies={props.searchResults}
           />
