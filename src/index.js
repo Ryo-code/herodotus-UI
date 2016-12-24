c59d10ce9f995f576751de8243754392ca834aa2
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-24 02:31:19 +0000

    Removed the registration route

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import App from './App';
 import './index.css';
 import SearchResultPage from './containers/SearchResultPage.jsx'
 import Home from './components/Home.js'
-import LandingPage from './components/LandingPage.jsx'
-import RegistrationPage from './containers/RegistrationPage.jsx'
+import LandingPage from './containers/LandingPage.jsx'
 import { browserHistory, Route, IndexRoute, Router } from 'react-router'
 // import { browserHistory, Link, Route, IndexRoute, Router } from 'react-router'
 
@@ -16,7 +15,6 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <IndexRoute component={Home}/>
         <Route path="/results" component={SearchResultPage} />
-        <Route path="/register" component={RegistrationPage} />
       </Route>
    </Router>,
   document.getElementById('root')
