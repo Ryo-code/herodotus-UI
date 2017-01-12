ed598ab24721e8fc9b6fff26194ded786e0229a5
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-01-12 16:23:09 +0000

    Added comments on the searchresultpage

--- src/containers/SearchResultPage.jsx
+++ src/containers/SearchResultPage.jsx
@@ -20,19 +20,21 @@ export default class SearchResultPage extends Component {
     showForm: false,
   }
 
-
   componentDidMount() {
     this.setState({movies: this.props.searchResults})
   }
 
+  // Selects the current movie when clicked
   selectMovie = (movie) => {
     this.setState({currentMovie: movie})
   }
 
+  // This will show the form when the button is clicked
   showSubmissionForm = () => {
     this.setState({showForm: true,})
   }
 
+  // This will close the form when clicked out of it
   handleClose = () => {
     this.setState({showForm: false,})
   }
@@ -54,10 +56,10 @@ export default class SearchResultPage extends Component {
       coverFlow =
       <div className="results">
         <div className="results-text">
-            <h4>Your search has yielded no results. </h4>
-            <p>Didn't find what you wanted? Add to the community! </p>
-            <RaisedButton label="Submit a New Movie" style={style} onClick={this.showSubmissionForm} />
-            {this.state.showForm ? <MovieSubmission showForm={this.state.showForm} handleClose={this.handleClose}/> : null}
+          <h4>Your search has yielded no results. </h4>
+          <p>Didn't find what you wanted? Add to the community! </p>
+          <RaisedButton label="Submit a New Movie" style={style} onClick={this.showSubmissionForm} />
+          {this.state.showForm ? <MovieSubmission showForm={this.state.showForm} handleClose={this.handleClose}/> : null}
         </div>
         <img src="http://gamingtrend.com/wp-content/screenshots/game-of-thrones-ascent/GOTA_Epic-Battle-Baratheon.jpg" alt="Smiley face"
          width="100%"/>
