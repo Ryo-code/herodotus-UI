2896c01e563fe17244aa172ebbb8b1e9b351c303
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-01-17 19:33:04 +0000

    Moved index.js

---
+++ src/app/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import SearchResultPage from './containers/SearchResultPage.jsx'
+import Home from './components/Home.js'
+import LandingPage from './containers/LandingPage.jsx'
+import { browserHistory, Route, IndexRoute, Router } from 'react-router'
+import FilmWall from './containers/FilmWall.jsx'
+// import { browserHistory, Link, Route, IndexRoute, Router } from 'react-router'
+
+ReactDOM.render(
+   <Router history={browserHistory}>
+
+      <Route path="/" component={App}>
+        <Route path="/" component={LandingPage}/>
+        <IndexRoute component={Home}/>
+        <Route path="/movies" component={FilmWall}/>
+        <Route path="/results" component={SearchResultPage} />
+      </Route>
+   </Router>,
+  document.getElementById('root')
+);
