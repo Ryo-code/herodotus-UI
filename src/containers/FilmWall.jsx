b5d6039e286242df563b9a35dafe32f53ec16c42
Ryo <kanadajinlee@yahoo.ca>
2017-01-19 01:10:42 -0500

    testing what it's like to push directly to heroku. mmm... feels good.

--- src/containers/FilmWall.jsx
+++ src/containers/FilmWall.jsx
@@ -90,7 +90,7 @@ class FilmWall extends Component {
         />
 
         <br/>
-        <hr  align="center" width="70%"/>
+        <hr align="center" width="70%"/>
           <p className="footer">The <i>Herodotus</i> web app comes from the creative minds of <a href="https://github.com/Jebbie87">Jeff Chang</a>, <a href="https://github.com/Ryo-code">Ryo MacPherson</a>, and <a href="https://github.com/SvemirskiHod">Joao Coelho</a>.</p>
       </div>
 
